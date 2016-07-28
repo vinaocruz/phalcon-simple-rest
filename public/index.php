4535ad32d5573c13f2f511970f4d172b7efe6c81
Vinicius Cruz <vinaocruz@gmail.com>
2016-07-27 23:16:33 -0300

    Configs

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../resource/config/main.php';
 require_once __DIR__ . '/../src/dependencies.php';
 
 $app = new \Phalcon\Mvc\Micro($di);
---
+++ resource/config/common.php
@@ -0,0 +1,7 @@
+<?php
+
+$config['api.version'] = "/v1";
+$config['api.endpoint'] = "/api";
+$config['api.timezone'] = 'America/Bahia';
+
+date_default_timezone_set($config['api.timezone']);
