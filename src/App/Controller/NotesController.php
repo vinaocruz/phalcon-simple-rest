6f3ee5d26cd8d7dcef01ef17dcab4f8351c989c8
Vinicius Cruz <vinaocruz@gmail.com>
2016-07-28 00:55:18 -0300

    More routes

--- src/App/Controller/NotesController.php
+++ src/App/Controller/NotesController.php
@@ -12,6 +12,10 @@ class NotesController extends AbstractController
         $collection->setPrefix($app['config']['api.endpoint'] . $app['config']['api.version'] . '/notes');
 
         $collection->get('/', 'fetchAll');
+        $collection->get('/{id}', 'fetch');
+        $collection->post('/', 'save');
+        $collection->put('/{id}', 'update');
+        $collection->delete('/{id}', 'delete');
 
         return $collection;
     }
@@ -22,4 +26,40 @@ class NotesController extends AbstractController
         return $this->response;
     }
 
+    public function fetch($id)
+    {
+        $this->response->setJsonContent($this->notes->get($id));
+        return $this->response;
+    }
+
+    public function save()
+    {
+        $note = $this->getDataFromRequest();
+
+        $this->response->setJsonContent(["id" => $this->notes->save($note)]);
+        return $this->response;
+
+    }
+
+    public function update($id)
+    {
+        $note = $this->getDataFromRequest();
+        $this->notes->update($id, $note);
+
+        $this->response->setJsonContent($note);
+        return $this->response;
+    }
+
+    public function delete($id)
+    {
+        $this->response->setJsonContent($this->notes->delete($id));
+        return $this->response;
+    }
+
+    public function getDataFromRequest()
+    {
+        return [
+            "note" => $this->request->getPost("note")
+        ];
+    }
 }
