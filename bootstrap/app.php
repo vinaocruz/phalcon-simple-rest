c64bbaaa0b727e1c5fd87b60ff9ddd0a17164432
edyonil <edyonil@gmail.com>
2016-10-27 10:05:12 -0300

    Modificação do skeleton e do docker-compose

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,25 @@
 <?php
 
-$di = new \Phalcon\Di\FactoryDefault();
+use Phalcon\Mvc\Micro;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
 
-$di['config'] = function() use ($config) {
-    return $config;
-};
+$di = new FactoryDefault();
 
-$di->set('notes', function () {
-    return new \App\Service\NotesService(new \App\Mapper\NotesMapper());
-});
+// Set up the database service
+$di->set(
+    "db",
+    function () {
+        return new PdoMysql(
+            [
+                "host"     => "localhost:33060",
+                "username" => "root",
+                "password" => "dev123",
+                "dbname"   => "my_app",
+            ]
+        );
+    }
+);
+
+// Create and bind the DI to the application
+$app = new Micro($di);
